53e70ac7c46ef8ec2235ace7423a489dcfb9b36a
Arnel Borres Go <arniel.go@gmail.com>
2024-04-23 22:21:53 +0800

    Add files via upload

---
+++ trds-visitor-counter-settings.php
@@ -0,0 +1,181 @@
+<?php
+
+/**
+ * TRDS Website Visitor Counter Settings Page
+ *
+ * This page allows users to customize the display options for the TRDS Website Visitor Counter plugin.
+ * Users can configure settings such as title text, colors, alignment, and labels for visitor counts.
+ */
+
+
+// Add settings page
+function custom_visitor_settings_page()
+{
+    add_options_page(
+        'TRDS Website Counter',
+        'TRDS Web Counter',
+        'manage_options',
+        'custom-visitor-settings',
+        'custom_visitor_settings_callback'
+    );
+}
+add_action('admin_menu', 'custom_visitor_settings_page');
+
+// Settings page callback function
+function custom_visitor_settings_callback()
+{
+?>
+    <div class="wrap">
+        <h2>TRDS Web Counter Settings</h2>
+        <form method="post" action="options.php">
+            <?php settings_fields('custom_visitor_settings_group'); ?>
+            <?php do_settings_sections('custom-visitor-settings'); ?>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+<?php
+}
+
+// Register and define settings
+function custom_visitor_register_settings()
+{
+    register_setting('custom_visitor_settings_group', 'custom_visitor_input');
+    register_setting('custom_visitor_settings_group', 'custom_visitor_input_color');
+    register_setting('custom_visitor_settings_group', 'custom_visitor_input_alignment');
+    register_setting('custom_visitor_settings_group', 'custom_visitor_views_text_today');
+    register_setting('custom_visitor_settings_group', 'custom_visitor_views_text_month');
+    register_setting('custom_visitor_settings_group', 'custom_visitor_views_color');
+    register_setting('custom_visitor_settings_group', 'custom_visitor_views_alignment');
+
+    add_settings_section(
+        'custom_visitor_settings_section',
+        'Visitor Count Display Options',
+        'custom_visitor_settings_section_callback',
+        'custom-visitor-settings'
+    );
+
+    add_settings_field(
+        'custom_visitor_input',
+        'Web Counter Title',
+        'custom_visitor_input_callback',
+        'custom-visitor-settings',
+        'custom_visitor_settings_section'
+    );
+
+    add_settings_field(
+        'custom_visitor_input_color',
+        'Web Counter Title Color',
+        'custom_visitor_input_color_callback',
+        'custom-visitor-settings',
+        'custom_visitor_settings_section'
+    );
+
+    add_settings_field(
+        'custom_visitor_input_alignment',
+        'Web Counter Title Alignment',
+        'custom_visitor_input_alignment_callback',
+        'custom-visitor-settings',
+        'custom_visitor_settings_section'
+    );
+
+    add_settings_field(
+        'custom_visitor_views_text_today',
+        'Views Today (Label)',
+        'custom_visitor_views_text_today_callback',
+        'custom-visitor-settings',
+        'custom_visitor_settings_section'
+    );
+
+    add_settings_field(
+        'custom_visitor_views_text_month',
+        'Views this month (Label)',
+        'custom_visitor_views_text_month_callback',
+        'custom-visitor-settings',
+        'custom_visitor_settings_section'
+    );
+
+    add_settings_field(
+        'custom_visitor_views_color',
+        'Label Color',
+        'custom_visitor_views_color_callback',
+        'custom-visitor-settings',
+        'custom_visitor_settings_section'
+    );
+
+    add_settings_field(
+        'custom_visitor_views_alignment',
+        'Label Alignment',
+        'custom_visitor_views_alignment_callback',
+        'custom-visitor-settings',
+        'custom_visitor_settings_section'
+    );
+}
+add_action('admin_init', 'custom_visitor_register_settings');
+
+// Section callback function
+function custom_visitor_settings_section_callback()
+{
+    echo '<p>To display visitor counts on any part of the site, Insert this shortcode : <code>[trds_custom_visitor_count]</code> into any post, page, or widget content. Save the changes and preview the page to see the visitor counts.</p>';
+}
+
+// Input callback function
+function custom_visitor_input_callback()
+{
+    $input = get_option('custom_visitor_input');
+    echo "<input type='text' name='custom_visitor_input' value='$input' />";
+}
+
+// Input color callback function
+function custom_visitor_input_color_callback()
+{
+    $input_color = get_option('custom_visitor_input_color', '#000000');
+    echo "<input type='color' name='custom_visitor_input_color' value='$input_color' />";
+}
+
+// Input alignment callback function
+function custom_visitor_input_alignment_callback()
+{
+    $input_alignment = get_option('custom_visitor_input_alignment', 'left');
+    $options = array('left', 'center', 'right');
+    echo "<select name='custom_visitor_input_alignment'>";
+    foreach ($options as $option) {
+        $selected = ($input_alignment == $option) ? 'selected' : '';
+        echo "<option value='$option' $selected>$option</option>";
+    }
+    echo "</select>";
+}
+
+// Views text today callback function
+function custom_visitor_views_text_today_callback()
+{
+    $views_text_today = get_option('custom_visitor_views_text_today', 'Views Today');
+    echo "<input type='text' name='custom_visitor_views_text_today' value='$views_text_today' />";
+}
+
+// Views text month callback function
+function custom_visitor_views_text_month_callback()
+{
+    $views_text_month = get_option('custom_visitor_views_text_month', 'Views This Month');
+    echo "<input type='text' name='custom_visitor_views_text_month' value='$views_text_month' />";
+}
+
+// Views color callback function
+function custom_visitor_views_color_callback()
+{
+    $views_color = get_option('custom_visitor_views_color', '#815b0a');
+    echo "<input type='color' name='custom_visitor_views_color' value='$views_color' />";
+}
+
+// Views alignment callback function
+function custom_visitor_views_alignment_callback()
+{
+    $views_alignment = get_option('custom_visitor_views_alignment', 'left');
+    $options = array('left', 'center', 'right');
+    echo "<select name='custom_visitor_views_alignment'>";
+    foreach ($options as $option) {
+        $selected = ($views_alignment == $option) ? 'selected' : '';
+        echo "<option value='$option' $selected>$option</option>";
+    }
+    echo "</select>";
+}
+?>
---
+++ trds-visitor-counter.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: TRDS Website Visitor Counter
+Description: This plugin counts daily and monthly visits and provides a shortcode to display the counts on any part of the site. It also allows customization of the visitor count display options.
+Version: 1.0
+Author: Arnel Go
+*/
+
+// Function to count daily visits
+function custom_visitor_count_daily()
+{
+    if (!isset($_COOKIE['custom_visitor_daily'])) {
+        $count = get_option('custom_visitor_daily_count', 0);
+        $count++;
+        update_option('custom_visitor_daily_count', $count);
+        setcookie('custom_visitor_daily', true, strtotime('tomorrow'));
+    }
+}
+
+// Function to count monthly visits
+function custom_visitor_count_monthly()
+{
+    if (!isset($_COOKIE['custom_visitor_monthly'])) {
+        $count = get_option('custom_visitor_monthly_count', 0);
+        $count++;
+        update_option('custom_visitor_monthly_count', $count);
+        setcookie('custom_visitor_monthly', true, strtotime('next month'));
+    }
+}
+
+// Function to display visitor counts using shortcode
+function custom_visitor_count_shortcode()
+{
+    $daily_count = get_option('custom_visitor_daily_count', 0);
+    $monthly_count = get_option('custom_visitor_monthly_count', 0);
+    $input_text = get_option('custom_visitor_input', 'Our Visitor');
+    $input_color = get_option('custom_visitor_input_color', '#000000');
+    $input_alignment = get_option('custom_visitor_input_alignment', 'left');
+    $views_text_today = get_option('custom_visitor_views_text_today', 'Views Today');
+    $views_text_month = get_option('custom_visitor_views_text_month', 'Views This Month');
+    $views_color = get_option('custom_visitor_views_color', '#815b0a');
+    $views_alignment = get_option('custom_visitor_views_alignment', 'left');
+
+    $output = '<div class="elementor-shortcode">';
+    $output .= '<div id="mvcwid" style="text-align: ' . $input_alignment . '; color: ' . $input_color . ';">';
+    $output .= '<h3 class="wps_visitor_title">' . $input_text . '</h3>';
+    //$output .= '<div id="wpsvccount">' . $daily_count . '</div>';
+    $output .= '<div id="wpsvctable">';
+    $output .= '<div id="wpsvcviews" style="text-align: ' . $views_alignment . '; color: ' . $views_color . ';">' . $views_text_today . ' : ' . $daily_count . '</div>';
+    $output .= '<div id="wpsvcviews" style="text-align: ' . $views_alignment . '; color: ' . $views_color . ';">' . $views_text_month . ' : ' . $monthly_count . '</div>';
+    $output .= '</div>';
+    $output .= '</div>';
+    $output .= '</div>';
+
+    return $output;
+}
+add_shortcode('trds_custom_visitor_count', 'custom_visitor_count_shortcode');
+
+// Hook to count daily and monthly visits
+add_action('init', 'custom_visitor_count_daily');
+add_action('init', 'custom_visitor_count_monthly');
+
+// Include backend configuration file
+require_once plugin_dir_path(__FILE__) . 'trds-visitor-counter-settings.php';
